7dfa602391dab628c659fb6990ec0b010611a265
William231143 <231143@shakopeeschools.org>
2022-05-05 09:37:50 -0500

    1.6b
    Added another 8 javascript features to my script.html file (functions are located in the script.js JS)

--- Version6.0/user08/JS/script.js
+++ Version6.0/user08/JS/script.js
@@ -1,17 +1,65 @@
 function changeText() {
-    document.getElementById("textChange").innerHTML="Thanks for liking my Webpage";
-   }
-  var space = " ";
-  var pos = 0;
-  var msg = "User 08";
-  
-  function Scroll() {
-    document.title =
-      msg.substring(pos, msg.length) + space + msg.substring(0, pos);
-  
-    pos++;
-    if (pos > msg.length) pos = 0;
-    window.setTimeout("Scroll()", 0);
+  document.getElementById("textChange").innerHTML =
+    "Thanks for liking my Webpage";
+}
+var space = " ";
+var pos = 0;
+var msg = "User 08";
+
+function Scroll() {
+  document.title =
+    msg.substring(pos, msg.length) + space + msg.substring(0, pos);
+
+  pos++;
+  if (pos > msg.length) pos = 0;
+  window.setTimeout("Scroll()", 0);
+}
+Scroll();
+
+function upperCase() {
+  var x = document.getElementById("fname");
+  x.value = x.value.toUpperCase();
+}
+function keyup() {
+  var x = document.getElementById("onRelease");
+  x.value = x.value.toUpperCase();
+}
+function hidden() {
+  document.getElementById("Secret").innerHTML = "I have appeared";
+}
+function whichElement(e) {
+  var targ;
+  if (!e) {
+    var e = window.event;
   }
-  Scroll();
-  
+  if (e.target) {
+    targ=e.target;
+  } else if (e.srcElement) {
+    targ=e.srcElement;
+  }
+  var tname;
+  tname = targ.tagName;
+  alert("You clicked on a " + tname + " element.");
+}
+function mouseclicking(elmnt, clr) {
+  elmnt.style.color = clr;
+}
+function inputcolor(x) {
+  x.style.background = "lightgreen";
+}
+function preferedBrowser() {
+  prefer = document.forms[0].browsers.value;
+  alert("You prefer browsing internet with " + prefer);
+}
+function confirmInput() {
+  random = document.forms[0].random.value;
+  alert("Hello " + random + "! You will now be redirected to the games");
+}
+var myWindow;
+function openWin() {
+  myWindow = window.open("", "", "width=400, height=200");
+}
+
+function closeWin() {
+  myWindow.close();
+}
